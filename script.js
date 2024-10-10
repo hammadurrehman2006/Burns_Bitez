6b7be29ccf0e44199e2965c4d89ba7aea76bd6a7
m_hammad-ur-rehman <hammadurrehman7707@gmail.com>
2024-10-10 15:42:42 +0500

    Responsive added

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const header = document.querySelector('header');
-window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky",window.scrollY>100)
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 100)
 })
 const video = document.querySelector('video');
 video.playbackRate = 1.0; // Adjust the speed as needed
+const menuButton = document.getElementById('menu-button');
+const navlist = document.getElementById('navlist');
+
+menuButton.addEventListener('click', () => {
+    navlist.classList.toggle('active');
+});
+
